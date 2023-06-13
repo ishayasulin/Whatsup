49ad2504e9a39c5159dc97e8f803f8a6a245db4c
ishayasulin <ishayasulin11@gmail.com>
2023-06-13 13:46:08 +0300

    design changes

--- app/src/main/java/com/example/whatsup/Login.java
+++ app/src/main/java/com/example/whatsup/Login.java
@@ -3,17 +3,17 @@ package com.example.whatsup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
-    private Button toRegister;
+    private TextView toRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        toRegister = (Button) findViewById(R.id.toRegister);
+        toRegister = (TextView) findViewById(R.id.toRegister);
         toRegister.setOnClickListener(this);
     }
 
--- app/src/main/java/com/example/whatsup/Register.java
+++ app/src/main/java/com/example/whatsup/Register.java
@@ -5,39 +5,35 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 public class Register extends AppCompatActivity implements View.OnClickListener {
-    private Button register;
+    private AppCompatButton register;
     private static final int PICK_IMAGE_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        register = (Button) findViewById(R.id.registerBtn);
+        register = (AppCompatButton) findViewById(R.id.registerBtn);
         register.setOnClickListener(this);
     }
     public void onImageButtonClick(View view) {
+        // Launch the file picker or image picker intent here
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, PICK_IMAGE_REQUEST_CODE);
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
-            // The user has selected an image
             Uri selectedImageUri = data.getData();
-
-            // Do something with the selected image URI
-            // For example, you can display the image in an ImageView
-            ImageView imageView = findViewById(R.id.imageView);
-            imageView.setImageURI(selectedImageUri);
+            ImageButton imageButton = findViewById(R.id.pictureRegister);
+            imageButton.setImageURI(selectedImageUri);
         }
     }
 
