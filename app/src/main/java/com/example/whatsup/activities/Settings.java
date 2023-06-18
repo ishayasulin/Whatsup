e3a400bf26484cabaac0c8d593c2d4b217fb8b6d
alondahan18 <alond2207@gmail.com>
2023-06-18 18:45:23 +0300

    now we can change to dark mode

--- app/src/main/java/com/example/whatsup/activities/Settings.java
+++ app/src/main/java/com/example/whatsup/activities/Settings.java
@@ -1,16 +1,43 @@
 package com.example.whatsup.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.whatsup.R;
 
 public class Settings extends AppCompatActivity {
 
+    private Switch switchMode;
+    private int currentNightMode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        switchMode = findViewById(R.id.switchMode);
+
+        // Get the current night mode
+        currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+
+        switchMode.setChecked(currentNightMode == Configuration.UI_MODE_NIGHT_YES);
+
+        switchMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked && currentNightMode != Configuration.UI_MODE_NIGHT_YES) {
+                    // Enable dark mode
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else if (!isChecked && currentNightMode != Configuration.UI_MODE_NIGHT_NO) {
+                    // Enable light mode
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
     }
 }
